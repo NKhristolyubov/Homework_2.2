53fd0da809216d2547ac2a4d183eb6567fa761b4
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-09-30 18:05:46 +0400

    Initial Commit

---
+++ Homework_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.2
+//
+//  Created by Николай Христолюбов on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
