1e71234a3b2b8ada9a091b5b4d5695c0527d5f5d
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-10-01 11:18:08 +0400

    unused UILabel have been removed optimized code

--- Homework_2.2/ViewController.swift
+++ Homework_2.2/ViewController.swift
@@ -8,11 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var viewMixColor: UIView!
     
-    @IBOutlet var redColorLabel: UILabel!
-    @IBOutlet var greenColorLabel: UILabel!
-    @IBOutlet var blueColorLabel: UILabel!
+    @IBOutlet var viewMixColor: UIView!
     
     @IBOutlet var redColorRangedLabel: UILabel!
     @IBOutlet var greenColorRangedLabel: UILabel!
@@ -22,35 +19,30 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorSlider: UISlider!
     @IBOutlet var blueColorSlider: UISlider!
     
-    private var redViewValue: CGFloat = 0
-    private var greenViewValue: CGFloat = 0
-    private var blueViewValue: CGFloat = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewMixColor.backgroundColor = UIColor(red: redViewValue, green: greenViewValue, blue: blueViewValue, alpha: 1)
-        redColorSlider.value = 0
-        greenColorSlider.value = 0
-        blueColorSlider.value = 0
+        viewMixColor.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(greenColorSlider.value),
+            alpha: 1)
     }
 
     @IBAction func redColorSliderAction() {
         redColorRangedLabel.text = String(format: "%.2f",redColorSlider.value)
-        redViewValue = CGFloat(redColorSlider.value)
-        viewMixColor.backgroundColor = UIColor(red: redViewValue, green: greenViewValue, blue: blueViewValue, alpha: 1)
+        
+        viewMixColor.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
     @IBAction func greenColorSliderAction() {
         greenColorRangedLabel.text = String(format: "%.2f",greenColorSlider.value)
-        greenViewValue = CGFloat(greenColorSlider.value)
-        viewMixColor.backgroundColor = UIColor(red: redViewValue, green: greenViewValue, blue: blueViewValue, alpha: 1)
+        viewMixColor.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
     @IBAction func blueColorSliderAction() {
         blueColorRangedLabel.text = String(format: "%.2f",blueColorSlider.value)
-        blueViewValue = CGFloat(blueColorSlider.value)
-        viewMixColor.backgroundColor = UIColor(red: redViewValue, green: greenViewValue, blue: blueViewValue, alpha: 1)
+        viewMixColor.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
     
     
